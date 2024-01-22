c8f9b6fb11e5e41572387b3758e99e21b6ce1fdb
Moise-Nturubika <nturumoussa@gmail.com>
2024-01-22 17:39:35 -0500

    Add notification route to app.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/LandingPage';
-// import SignUp from './Components/Sign_Up/Sign_Up';
-// import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
 import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 import DoctorCard from './Components/DoctorCard/DoctorCard';
 import BookingConsultation from './Components/BookingConsultation';
 import Notification from './Components/Notification/Notification';
 import Login from './Components/Login/Login';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
 
 function App() {
 
@@ -21,7 +20,8 @@ function App() {
           <Navbar/>
               <Routes>
                 <Route path="/" element={<Landing_Page/>}/>
-                <Route path="/login" element={<Login/>}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/signup" element={<Sign_Up />}/>
                 <Route path="/instant-consultation" element={<InstantConsultation />} />
                 <Route path="/find-doctor" element={<FindDoctorSearch />} />
                 <Route path="/card-doctor" element={<DoctorCard />} />
