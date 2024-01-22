0f64d2907507d55284da91576cd33f16a7e424b9
Moise-Nturubika <nturumoussa@gmail.com>
2024-01-22 16:26:46 -0500

    Add appointment form

--- src/Components/DoctorCard/DoctorCard.js
+++ src/Components/DoctorCard/DoctorCard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import './DoctorCard.css';
+import AppointmentForm from '../AppointmentForm/AppontmentForm';
 import { v4 as uuidv4 } from 'uuid';
 
 
@@ -51,10 +52,38 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
 
       <div className="doctor-card-options-container">
       </div>
-      <div>  <button className='book-appointment-btn'>                    
+      <div className="doctorbg" style={{ height: '100vh', overflow: 'scroll' }}>
+              <div>
+                <div className="doctor-card-profile-image-container">
+                <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> </svg>
+                </div>
+                <div className="doctor-card-details">
+                  <div className="doctor-card-detail-name">{name}</div>
+                  <div className="doctor-card-detail-speciality">{speciality}</div>
+                  <div className="doctor-card-detail-experience">{experience} years experience</div>
+                  <div className="doctor-card-detail-consultationfees">Ratings: {ratings}</div>
+                </div>
+              </div>
+
+              {appointments.length > 0 ? (
+                <>
+                  <h3 style={{ textAlign: 'center' }}>Appointment Booked!</h3>
+                  {appointments.map((appointment) => (
+                    <div className="bookedInfo" key={appointment.id}>
+                      <p>Name: {appointment.name}</p>
+                      <p>Phone Number: {appointment.phoneNumber}</p>
+                      <button onClick={() => handleCancel(appointment.id)}>Cancel Appointment</button>
+                    </div>
+                  ))}
+                </>
+              ) : (
+                <AppointmentForm doctorName={name} doctorSpeciality={speciality} onSubmit={handleFormSubmit} />
+              )}
+            </div>
+      {/* <div>  <button className='book-appointment-btn'>                    
             <div>Book Appointment</div>
           <div>No Booking Fee</div>
-        </button> </div>
+        </button> </div> */}
          
     </div>
   );
